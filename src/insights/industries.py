f8473850a9a2cdf4f4ecfc5600d4879873064e1d
André Santos <x_sode@hotmail.com>
2023-01-24 08:36:51 -0300

    'chore: get_unique_industires function, task 3'

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,18 @@
 from typing import List, Dict
+from jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    read_data = read(path)
+    all_industries = []
+    for row in read_data:
+        if row["industry"] and row["industry"] not in all_industries:
+            all_industries.append(row["industry"])
 
-    Must call `read`
+    return all_industries
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
 
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+get_unique_industries("data/jobs.csv")
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
