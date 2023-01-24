adffeb19ee426a0a36532e6acd09bd25936b1cb7
André Santos <x_sode@hotmail.com>
2023-01-24 09:12:36 -0300

    'chore: filter_by_job_type function implementation'

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -28,18 +28,13 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
+    data = read(jobs)
+    filtered_job_types = []
+    for row in data:
+        if row["job_type"] == job_type:
+            filtered_job_types.append(row)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    return filtered_job_types
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+
+# filter_by_job_type('data/jobs.csv', 'FULL_TIME')
