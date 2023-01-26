3eedbd93c1edfe55e8484a15826b7f9982a9286f
André Santos <x_sode@hotmail.com>
2023-01-26 12:29:47 -0300

    'test: testing the number of times that the words python and javascript is used'

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+    word = "Python"
+    word_2 = "Javascript"
+    result = count_ocurrences(path, word)
+    result_2 = count_ocurrences(path, word_2)
+    assert result == 1639
+    assert result_2 == 122
