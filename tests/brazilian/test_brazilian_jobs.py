1ce01da291854d1355d6e00892489ec6ba05334f
André Santos <x_sode@hotmail.com>
2023-01-26 13:11:56 -0300

    'test: testing brazilian_jobs'

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    portuguese_file_path = "tests/mocks/brazilians_jobs.csv"
+    english_result = read_brazilian_file(portuguese_file_path)
+
+    assert "title" in english_result[0].keys()
+    assert "salario" not in english_result[0].keys()
