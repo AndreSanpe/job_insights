330ecbbceba4a7335fee9c1c7d6d5fc577425b6a
André Santos <x_sode@hotmail.com>
2023-01-26 13:58:33 -0300

    'test: test_sort_by_criteria function'

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,11 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+import src.insights.jobs as jobs
 
 
 def test_sort_by_criteria():
-    pass
+    job_list = jobs.read("data/jobs.csv")
+    sort_by(job_list, "min_salary")
+    print(job_list[1]["min_salary"])
+    print(job_list[4]["min_salary"])
+
+    assert int(job_list[3]["min_salary"]) >= int(job_list[4]["min_salary"])
